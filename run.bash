80ce29553a097c7b39c7f23594fd4baf896166b0
DongZhouGu <gdz678@163.com>
2020-11-01 22:44:02 +0800

    update

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
-git pull origin master
-git add .
-git commit  -m  "atuosave"
-git push -u origin master
+git remote add origin git@github.com:DongZhouGu/wjx-academic.git
+git add  .
+git commit  -m  "update"
+git push origin master
