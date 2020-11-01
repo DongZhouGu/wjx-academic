81e2819930d52e5ef8a345f421b541f4ce4d46cc
DongZhouGu <gdz678@163.com>
2020-11-01 22:36:39 +0800

    atuosave

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ from selenium.webdriver.chrome.options import Options
 import time
 from time import ctime, mktime
 import yaml
-import pyautogui
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 
 class submit():
     def __init__(self, browser, questions):
         self.browser = browser
-        self.browser.maximize_window()
         self.questions = questions
 
     def re_random(self, num, div_string):
@@ -35,7 +36,7 @@ class submit():
         for i in range(len(self.questions)):
             div_string = self.questions[i].find_element_by_class_name('field-label').get_attribute("innerHTML")
             self.re_random(i, div_string)
-        self.browser.find_element_by_id('ctlNext').click()
+        # self.browser.find_element_by_id('ctlNext').click()
         time.sleep(0.5)
         print(f'时间: {ctime()}==== {info["name"]}提交成功！')
 
@@ -43,14 +44,8 @@ class submit():
         answers = self.questions[num].find_elements_by_class_name('ui-radio')
         for answer in answers:
             text = answer.text
-            if '20' or '男' in text:
+            if '20' in text or '男' in text:
                 return answer.click()
-
-    # def submit_multi_choice(self, question_number, choose_list):  # 多选题
-    #     pass
-    #     answer = self.questions[question_number].find_elements_by_css_selector('li')
-    #     for i in range(len(choose_list)):
-    #         answer[choose_list[i]].click()
 
     def submit_text(self, question_number, text):  # 填空题
         text_area = self.questions[question_number].find_element_by_css_selector('input')
@@ -67,25 +62,31 @@ def load_config(config_path):
 if __name__ == "__main__":
     config = load_config("setting_config.yaml")
     url = config['url']
+    index = url.find('.aspx&response_type')
+    url = url[:index - 8] + config['wjx_id'] + url[index:]
     target_time = config['target_time']
     useragent = config['user-agent']
-    cookie=config['cookies']
     chrome_options = Options()
     chrome_options.add_argument(f'--user-agent={useragent}')
     # chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--disable-gpu')
     browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=chrome_options)
-    # browser.add_cookie(cookie)
-
     target_time = mktime(time.strptime(target_time, "%Y-%m-%d %H:%M:%S"))
+
     for i, info in enumerate(config['users']):
+        browser.get(url)
+        time.sleep(1)
+        yes = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'btnOk')))
         while (True):
             if (time.time() > target_time):
-                browser.get(url)
-                questions = browser.find_elements_by_class_name('field.ui-field-contain')
-                if (len(questions) != 0): break
+                yes.click()
+                break
             else:
                 print(f'时间: {ctime()}====等待中')
         print(f'时间: {ctime()}===={info["name"]}开始提交')
+        questions = browser.find_elements_by_css_selector('.field')
+        while (len(questions) == 0):
+            questions = browser.find_elements_by_css_selector('.field')
         submit(browser, questions).post(info)
+
     print(f'时间: {ctime()}====任务完成')
---
+++ run.bash
@@ -0,0 +1,4 @@
+git pull origin master
+git add .
+git commit  -m  "atuosave"
+git push -u origin master
