65fdd45d74d0476f101560a51531c6617efe8aef
DongZhouGuu <gdz678@163.com>
2021-10-10 23:59:09 +0800

    update

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 @IDE ：PyCharm
 """
 import asyncio
+import re
+
 from pyppeteer import launch
 import time
 from time import ctime, mktime
@@ -20,50 +22,95 @@ def load_config(config_path):
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
-async def submit_choice(questions, num):
-    answers = await questions[num].querySelectorAll('.ui-radio')
-    for answer in answers:
+async def submit_choice(q, choice):
+    answers = await q.querySelectorAll('.ui-radio')
+    flag = False
+    for i, answer in enumerate(answers):
         text = await (await answer.getProperty('textContent')).jsonValue()
-        if '20' in text or '男' in text or '研一' in text:
-            return await answer.click()
+        if choice in text:
+            flag = True
+            break
+    if flag:
+        return await answers[i].click()
+    # 都没匹配到 随便选第一个
+    else:
+        return await answers[0].click()
 
 
-async def submit_text(questions, num, text):  # 填空题
-    text_area = await questions[num].querySelector('input')
+async def submit_text(q, text):  # 填空题
+    text_area = await q.querySelector('input')
     await text_area.type(text)
 
 
-async def is_choice(questions, num):
-    answers = await questions[num].querySelectorAll('.ui-radio')
-    if answers == None:
-        return False
+async def submit_code(q, div_string, keyword):  # 特殊人工验证
+    text_area = await q.querySelector('input')
+    i = div_string.rfind(keyword) + len(keyword)
+    j = max(div_string.rfind("("), div_string.rfind("（"), -1)
+
+    res = []
+    res.append(re.compile(r'"(.*)"').findall(div_string[i:j]))
+    res.append(re.compile(r'“(.*)”').findall(div_string[i:j]))
+    res.append(re.compile(r'"(.*)”').findall(div_string[i:j]))
+    res.append(re.compile(r'“(.*)"').findall(div_string[i:j]))
+    flag = False
+    for i, r in enumerate(res):
+        if r:
+            flag = True
+            break
+    if flag:
+        await text_area.type(res[i][0])
     else:
-        await answers[0].click()
-        return True
+        num = re.findall(r'[1-9]+\.?[0-9]*', div_string[i:j])
+        await text_area.type(num[0])
+
+
+async def main_logic_text(q, div_string):
+    if "姓名" in div_string or "名字" in div_string:
+        await submit_text(q, info["name"])
+    elif "联系方式" in div_string or "手机" in div_string or "电话" in div_string:
+        await submit_text(q, info["phone"])
+    elif "学号" in div_string:
+        await submit_text(q, info["id"])
+    elif "身份证" in div_string:
+        await submit_text(q, info["card_id"])
+    elif "专业" in div_string:
+        await submit_text(q, info["major"])
+    elif "年级" in div_string:
+        await submit_text(q, info["grade"])
+    elif "性别" in div_string:
+        await submit_text(q, info["gender"])
+    elif "输入" in div_string:
+        await submit_code(q, div_string, "输入")
+    elif "填写" in div_string:
+        await submit_code(q, div_string, "填写")
+    else:
+        await submit_text(q, "1")
+
+
+async def main_logic_choice(q, div_string):
+    if "专业" in div_string:
+        await submit_choice(q, info["major"])
+    elif "年级" in div_string:
+        await submit_choice(q, info["grade"])
+    elif "性别" in div_string:
+        await submit_choice(q, info["gender"])
+    else:
+        answers = await q.querySelectorAll('.ui-radio')
+        answers[0].click()
 
 
 async def submit(page, questions, info):
-    for num in range(len(questions)):
-        element = await questions[num].querySelector(f'#div{num + 1} > div.field-label')
+    for i, q in enumerate(questions):
+        element = await q.querySelector(f'#div{i + 1} > div.field-label')
         div_string = await (await element.getProperty('textContent')).jsonValue()
-        if "姓名" in div_string or "名字" in div_string:
-            await submit_text(questions, num, info["name"])
-        elif "联系方式" in div_string or "手机" in div_string or "电话" in div_string:
-            await submit_text(questions, num, info["phone"])
-        elif "学号" in div_string:
-            await submit_text(questions, num, info["id"])
-        elif "身份证" in div_string:
-            await submit_text(questions, num, info["card_id"])
-        elif "专业" in div_string:
-            await submit_text(questions, num, info["major"])
-        elif "年级" in div_string:
-            await submit_choice(questions, num)
-        elif "性别" in div_string:
-            await submit_choice(questions, num)
-        else:
-            if (await is_choice(questions, num) == False):
-                await submit_text(questions, num, "1")
-    # time.sleep(100)
+        # 是填空题
+        if await q.querySelectorAll('.ui-input-text'):
+            await main_logic_text(q, div_string)
+        # 是选择题
+        elif await q.querySelectorAll('.ui-radio'):
+            await main_logic_choice(q, div_string)
+
+    time.sleep(0.5)
     submitb = await page.querySelector('#ctlNext')
     await submitb.click()
     time.sleep(1)
@@ -92,7 +139,7 @@ async def main():
     page = await browser.newPage()
     await page.setUserAgent(useragent)
     target_time = mktime(time.strptime(target_time, "%Y-%m-%d %H:%M:%S"))
-
+    global info
     for i, info in enumerate(config['users']):
         while (True):
             if (time.time() > target_time):
@@ -107,9 +154,9 @@ async def main():
                         # await asyncio.wait([
                         #     page.querySelectorAll('.field'), page.waitForNavigation({'timeout': 50000}), ])
                         await asyncio.wait([
-                            page.querySelectorAll('.field') ])
+                            page.querySelectorAll('.field')])
                         questions = await page.querySelectorAll('.field')
-                        print(questions)
+                        # print(questions)
                         if (len(questions) != 0): break
                 else:
                     questions = await page.querySelectorAll('.field')
